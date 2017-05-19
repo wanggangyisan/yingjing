0dd1756a4d717c854d1323d057b67da83a07f75e
suiyuan <wg91@gzhxyc.com>
2017-05-19 18:01:20 +0800

    更改数据

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -11,9 +11,9 @@ class Index extends Controller
     {
         //调用模型获取数据
         $model = new IndexModel();
-        $data = $model->getAll();
-        var_dump($data);
-         return $this->fetch();
+        $data = $model->getAll(1);
+        $this->assign('data',$data);
+        return $this->fetch();
     }
     /*文件上传页面*/
     public function form_test()
@@ -40,11 +40,13 @@ class Index extends Controller
                 if($file){
                     /*获取文件内容*/
                     $content = file_get_contents($move_path);
+                    /*调用字符转换的方法*/
+                    $replace_content = $this->replace_content(iconv('GBK','UTF-8//IGNORE',$content));
                     /*拼装数据保存道数据库*/
                     $arr = array(
                         'filename'  => $data['name'],/*原文件名*/
                         'title'     => '文章标题',/*文章标题*/
-                        'content'   => iconv('GBK','UTF-8//IGNORE',$content),/*文件内容,gbk格式编码转换成utf-8*/
+                        'content'   => $replace_content,/*文件内容,gbk格式编码转换成utf-8*/
                         'file_path' => $file_name,/*文件上传后保存的路径*/
                         'upload_time' => time(),/*文件上传的时间*/
                     );
@@ -67,10 +69,35 @@ class Index extends Controller
         }
 
     }
+    /*字符转换*/
+    public function replace_content($str)
+    {
+        $count = strlen($str);
+        if(!$count){
+            return $str;
+        }
+        /*〓替换成空格*/
+        if(strpos('',$str)){
+            $replace = str_replace('〓','&nbsp;',$str);
+        }
+
+
+    }
     /*修改文章内容*/
     public function update_content()
     {
-        echo  '文章修改';
+        /*根据ID查询文章内容*/
+        $id = input("file_id");
+        /*实例化模型*/
+        $model = new IndexModel();
+        $data = $model->get_id($id);
+        $this->assign('data',$data);
+        return $this->fetch();
     }
-    /**/
+    /*接收修改后内容*/
+    public function get_content()
+    {
+        $data = input();
+        return $data;
+    }
 }
--- application/index/model/Index.php
+++ application/index/model/Index.php
@@ -21,10 +21,25 @@ class Index extends Model
         $data = Db::name('file_info')->insertGetId($arr);
         return $data;
     }
-    /*获取分页数据或者查询数据*/
-    public function getAll()
+    /*获取分页数据或者查询数据  $index+page 当前页  每页显示20条数据*/
+    public function getAll($index_page)
     {
-        $data = Db::name('file_info')->page(1,20)->select();
+        $data = Db::name('file_info')
+            ->field('id,filename,title,upload_time')
+            ->page($index_page,20)
+            ->order('id desc')
+            ->select();
         return $data;
+    }
+    /*根据ID获取数据*/
+    public function get_id($id)
+    {
+        $data = Db::name('file_info')->where(array('id'=>$id))->find();
+        return $data;
+    }
+    /*修改数据*/
+    public function save_data()
+    {
+        $res = Db::name('file_info')->update();
     }
 }
